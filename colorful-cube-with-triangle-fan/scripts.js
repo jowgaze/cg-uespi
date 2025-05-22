a97ed2d297fd353023630de5494c7c519a7848f5
jowgaze <jonatha.biomon.hp@gmail.com>
2025-05-22 10:21:04 -0300

    Feat: Add rotation, scale and translate

--- colorful-cube-with-triangle-fan/scripts.js
+++ colorful-cube-with-triangle-fan/scripts.js
@@ -1,9 +1,24 @@
 "use strict"
-
 var gl;
 var vertices = getVertices();
 var indices = getIndices(vertices);
 var colors = getColors();
+
+var modelViewMatrixLoc;
+var theta = [0, 0, 0];
+var rotateStatus = [false, false, false];
+const scaleCube = document.getElementById("scale");
+const translateX = document.getElementById("translateX");
+const translateY = document.getElementById("translateY");
+
+document.getElementById("buttonX")
+    .addEventListener("click", () => rotateStatus[0] = !rotateStatus[0]);
+
+document.getElementById("buttonY")
+    .addEventListener("click", () => rotateStatus[1] = !rotateStatus[1]);
+
+document.getElementById("buttonZ")
+    .addEventListener("click", () => rotateStatus[2] = !rotateStatus[2]);
 
 init();
 
@@ -40,10 +55,30 @@ function init() {
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW)
 
+    modelViewMatrixLoc = gl.getUniformLocation(program, "uModelViewMatrix");
+
     render();
 }
 
 function render() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    var modelViewMatrix = mat4();
+
+    for (let i = 0; i <= 2; i++)
+        if (rotateStatus[i])
+            theta[i] += 2;
+
+    modelViewMatrix = mult(modelViewMatrix, scale(scaleCube.value, scaleCube.value, scaleCube.value));
+    
+    modelViewMatrix = mult(modelViewMatrix, translate(translateX.value, translateY.value, 1));
+
+    modelViewMatrix = mult(modelViewMatrix, rotateX(theta[0]));
+    modelViewMatrix = mult(modelViewMatrix, rotateY(theta[1]));
+    modelViewMatrix = mult(modelViewMatrix, rotateZ(theta[2]));
+
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+
     gl.drawElements(gl.TRIANGLE_FAN, indices.length, gl.UNSIGNED_BYTE, 0);
+    requestAnimationFrame(render);
 }
